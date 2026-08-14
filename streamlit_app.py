97aa9cc41fd04924eea1c41811a65b5984b67cd3
rachela2455 <rachela2455@gmail.com>
2026-08-14 16:09:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # temporary test
-st.write("Secrets keys:", list(st.secrets.keys()))
+st.write("Connection keys:", list(st.secrets["connections"].keys()))
 
 # Write directly to the app
 st.title(f"Customise Your Smoothie! :cup_with_straw:")

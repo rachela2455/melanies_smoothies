9c0d4e07f9106df862b62a58f378f1ce622f80de
rachela2455 <rachela2455@gmail.com>
2026-08-17 11:12:02 +0100

    Update streamlit_app.py
    undo amendments

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ if ingredients_list:
 
 # New section to display smoothiefoot nutrition information
 import requests  
-smoothiefroot_response = requests.get("[https://www.smoothiefroot.com/api/fruit/watermelon](https://www.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
